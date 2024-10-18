9e2eff83643e722e08d6ea001bf23356b9a0913c
Barap1 <140808362+Barap1@users.noreply.github.com>
2024-10-18 19:05:12 -0400

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Cybersecurity Passionate",
+          "Computer Science Mentor",
+          "Machine Learning Enthusiast",
         ],
         autoStart: true,
         loop: true,
