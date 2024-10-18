ba7525cec9ffe6d493d4a0d7b2b2496ee54c2d83
Barap1 <140808362+Barap1@users.noreply.github.com>
2024-10-18 19:54:07 -0400

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
+//import Toolstack from "./Toolstack";
 
 function About() {
   return (
