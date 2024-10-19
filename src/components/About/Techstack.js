a569be72c50985e362a81bb08fe97f4b33b9ed97
Barap1 <140808362+Barap1@users.noreply.github.com>
2024-10-18 20:05:17 -0400

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
+import { SiFlask } from "react-icons/si";
+import { SiBurpsuite } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { SiKalilinux } from "react-icons/si";
+import { SiTensorflow } from "react-icons/si";
+
 import {
   DiJavascript1,
   DiReact,
@@ -23,46 +29,43 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
+        <DiReact />
+      </Col> 
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col> 
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col> 
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col> 
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlask />
+      </Col> 
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBurpsuite />
+      </Col> 
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiKaliLinux />
+      </Col> 
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTensorFlow />
       </Col>
     </Row>
   );
